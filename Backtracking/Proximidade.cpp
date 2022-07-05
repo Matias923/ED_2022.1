401405f6916e8fdb3559e4b3459aaa9fe4b8e1be
Matias Moraes <67382741+Matias923@users.noreply.github.com>
2022-07-05 10:20:51 -0300

    .

---
+++ Backtracking/Proximidade.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+    string distancia;
+
+    cin >> distancia;
+    int tamanho = sizeof(distancia);
+    cout << tamanho;
+    return 0;
+}
